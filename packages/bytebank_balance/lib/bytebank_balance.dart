8e0141801549fc8f70b15bab1b0fecaad6e5ae9f
Marco.Gomes <Marco.Gomes@sicoob.com.br>
2024-09-19 10:54:51 -0300

    Implementando comunicação com a API para através de um PIN exibir o saldo. E addicionando package do git.

--- packages/bytebank_balance/lib/bytebank_balance.dart
+++ packages/bytebank_balance/lib/bytebank_balance.dart
@@ -1,7 +1,10 @@
 library bytebank_balance;
+import 'package:bytebank_balance/service/balance_service.dart';
 import 'package:flutter/material.dart';
+import 'widgets/type_pin_dialog.dart';
 
 part 'widgets/balance_widget.dart';
+
 
 /// A Calculator.
 class Calculator {
--- packages/bytebank_balance/lib/widgets/balance_widget.dart
+++ packages/bytebank_balance/lib/widgets/balance_widget.dart
@@ -1,24 +1,23 @@
-
-
 part of '../bytebank_balance.dart';
-
 
 class BytebankBalance extends StatefulWidget {
   final Color color;
-  const BytebankBalance({super.key, required this.color});
+  final String userId;
+
+  const BytebankBalance({super.key, required this.color, required this.userId});
 
   @override
   State<BytebankBalance> createState() => _BytebankBalanceState();
 }
 
 class _BytebankBalanceState extends State<BytebankBalance> {
-
   bool isShowingBalance = false;
   double userBalance = 0;
+  BalanceService balanceService = BalanceService();
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -33,9 +32,14 @@ class _BytebankBalanceState extends State<BytebankBalance> {
               ),
             ),
             const SizedBox(width: 24),
-            IconButton(onPressed: (){
-              onVisibilityBalanceClicked();
-            }, icon: Icon((isShowingBalance) ? Icons.visibility : Icons.visibility_off, color:  widget.color,))
+            IconButton(
+                onPressed: () {
+                  onVisibilityBalanceClicked();
+                },
+                icon: Icon(
+                  (isShowingBalance) ? Icons.visibility : Icons.visibility_off,
+                  color: widget.color,
+                ))
           ],
         ),
         Padding(
@@ -50,7 +54,9 @@ class _BytebankBalanceState extends State<BytebankBalance> {
           ),
         ),
         Text(
-          (isShowingBalance) ? "R\$ ${userBalance.toStringAsFixed(2)}" : "R\$ ****",
+          (isShowingBalance)
+              ? "R\$ ${userBalance.toStringAsFixed(2)}"
+              : "R\$ ****",
           style: TextStyle(
             color: widget.color,
             fontSize: 32,
@@ -60,18 +66,39 @@ class _BytebankBalanceState extends State<BytebankBalance> {
     );
   }
 
-  onVisibilityBalanceClicked(){
-
-    if(isShowingBalance){
+  onVisibilityBalanceClicked() {
+    if (isShowingBalance) {
       setState(() {
         isShowingBalance = false;
       });
-    }else{
-      setState(() {
-      isShowingBalance = true;
+    } else {
+      balanceService.hasPin(userId: widget.userId).then((bool hasPin) {
+        if (hasPin) {
+          showPinDialog(context, isRegister: false).then((String? pin) {
+            if (pin != null) {
+              balanceService
+                  .getBalance(userId: widget.userId, pin: pin)
+                  .then((double balance) {
+                setState(() {
+                  isShowingBalance = true;
+                  userBalance = balance;
+                });
+              });
+            }
+          });
+        } else {
+          showPinDialog(context, isRegister: true).then((String? pin) {
+            if (pin != null) {
+              balanceService
+                  .createPin(userId: widget.userId, pin: pin)
+                  .then((double balance) {
+                isShowingBalance = true;
+                userBalance = balance;
+              });
+            }
+          });
+        }
       });
     }
-
   }
 }
-
